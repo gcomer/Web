0e0279c8ee15eb6742c557b590987c23d4b9d783
Gabriel Comer <gabe.comer7@gmail.com>
2019-02-19 19:09:13 +0000

    Added site files

---
+++ sidebar.js
@@ -0,0 +1,9 @@
+function openNav() {
+  document.getElementsByClassName('sidebar')[0].style.visibility = "visible";
+  document.getElementsByClassName('openbutton')[0].style.visibility = "hidden";
+}
+
+function closeNav() {
+  document.getElementsByClassName('sidebar')[0].style.visibility = "hidden";
+  document.getElementsByClassName('openbutton')[0].style.visibility = "visible";
+}
